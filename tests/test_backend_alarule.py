8c8641894b055d5df9800307394dd596354d58ae
tungdr4 <tungdr4@gmail.com>
2022-09-30 14:21:30 +0700

    Update tests

--- tests/test_backend_alarule.py
+++ tests/test_backend_alarule.py
@@ -9,7 +9,7 @@ def alarule_backend():
 
 
 # TODO: implement tests for some basic queries and their expected results.
-def test_alarule_and_expression(alarule_backend: alaruleBackend):
+def test_KQL_and_expression(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -18,21 +18,22 @@ def test_alarule_and_expression(alarule_backend: alaruleBackend):
             status: test
             logsource:
                 category: test_category
-                product: taskscheduler
-                source: taskscheduler
+                product: Test_Product
+                source: Test_Product
             detection:
                 sel:
                     EventID: valueA
                     fieldB: valueB
                 condition: sel
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == ['Test_Product\n| where fieldA=="valueA" and fieldB=="valueB"']
+        == 'Test_Product\n| where (event_id=="valueA" and fieldB=="valueB")'
     )
 
 
-def test_alarule_or_expression(alarule_backend: alaruleBackend):
+def test_KQL_or_expression(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -49,13 +50,14 @@ def test_alarule_or_expression(alarule_backend: alaruleBackend):
                     fieldB: valueB
                 condition: 1 of sel*
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == ['Test_Product\n| where fieldA=="valueA" or fieldB=="valueB"']
+        == 'Test_Product\n| where fieldA=="valueA" or fieldB=="valueB"'
     )
 
 
-def test_alarule_and_or_expression(alarule_backend: alaruleBackend):
+def test_KQL_and_or_expression(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -75,15 +77,14 @@ def test_alarule_and_or_expression(alarule_backend: alaruleBackend):
                         - valueB2
                 condition: sel
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == [
-            'Test_Product\n| where ((fieldA in ("valueA1", "valueA2")) and (fieldB in ("valueB1", "valueB2")))'
-        ]
+        == 'Test_Product\n| where ((fieldA in ("valueA1", "valueA2")) and (fieldB in ("valueB1", "valueB2")))'
     )
 
 
-def test_alarule_or_and_expression(alarule_backend: alaruleBackend):
+def test_KQL_or_and_expression(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -102,15 +103,14 @@ def test_alarule_or_and_expression(alarule_backend: alaruleBackend):
                     fieldB: valueB2
                 condition: 1 of sel*
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == [
-            'Test_Product\n| where (fieldA=="valueA1" and fieldB=="valueB1") or (fieldA=="valueA2" and fieldB=="valueB2")'
-        ]
+        == 'Test_Product\n| where (fieldA=="valueA1" and fieldB=="valueB1") or (fieldA=="valueA2" and fieldB=="valueB2")'
     )
 
 
-def test_alarule_in_expression(alarule_backend: alaruleBackend):
+def test_KQL_in_expression(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -128,15 +128,14 @@ def test_alarule_in_expression(alarule_backend: alaruleBackend):
                         - valueC*
                 condition: sel
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == [
-            'Test_Product\n| where fieldA=="valueA" or fieldA=="valueB" or fieldA startswith "valueC"'
-        ]
+        == 'Test_Product\n| where fieldA=="valueA" or fieldA=="valueB" or fieldA startswith "valueC"'
     )
 
 
-def test_alarule_regex_query(alarule_backend: alaruleBackend):
+def test_KQL_regex_query(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -152,13 +151,14 @@ def test_alarule_regex_query(alarule_backend: alaruleBackend):
                     fieldB: foo
                 condition: sel
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == ['Test_Product\n| where (fieldA matches regex "foo.*bar" and fieldB=="foo")']
+        == 'Test_Product\n| where (fieldA matches regex "foo.*bar" and fieldB=="foo")'
     )
 
 
-def test_alarule_cidr_query(alarule_backend: alaruleBackend):
+def test_KQL_cidr_query(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -173,13 +173,14 @@ def test_alarule_cidr_query(alarule_backend: alaruleBackend):
                     field|cidr: 192.168.0.0/16
                 condition: sel
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == ['Test_Product\n| where ipv4_is_in_range(field, "192.168.0.0/16")']
+        == 'Test_Product\n| where ipv4_is_in_range(field, "192.168.0.0/16")'
     )
 
 
-def test_alarule_field_name_with_whitespace(alarule_backend: alaruleBackend):
+def test_KQL_field_name_with_whitespace(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -194,13 +195,14 @@ def test_alarule_field_name_with_whitespace(alarule_backend: alaruleBackend):
                     field name: value fajweio
                 condition: sel
         """
-            )
+            ),
+            output_format='KQL',
         )
-        == ['Test_Product\n| where ["field name"]=="value"']
+        == 'Test_Product\n| where ["field name"]=="value fajweio"'
     )
 
 
-def test_alarule_field_name_dynamic(alarule_backend: alaruleBackend):
+def test_KQL_field_name_dynamic(alarule_backend: alaruleBackend):
     assert (
         alarule_backend.convert(
             SigmaCollection.from_yaml(
@@ -221,7 +223,7 @@ def test_alarule_field_name_dynamic(alarule_backend: alaruleBackend):
             ),
             output_format='KQL',
         )
-        == ['Test_Product\n| where ["field name"]=="value"']
+        == 'Test_Product\n| where parse_json(Field1)["value1"]=="value fajweio"'
     )
 
 
@@ -246,28 +248,5 @@ def test_alarule_field_name_dynamic(alarule_backend: alaruleBackend):
             ),
             output_format='ala_rule',
         )
-        == [
-            '{"displayName": "Test", "description": "None Technique: .", "severity": "medium", "enabled": true, "query": "Test_Product\\n| where parse_json(Field1)[\\"value1\\"]==\\"value fajweio\\"", "queryFrequency": "PT30M", "queryPeriod": "PT30M", "triggerOperator": "GreaterThan", "triggerThreshold": 0, "suppressionDuration": "PT2H30M", "suppressionEnabled": true, "tactics": [], "techniques": []}'
-        ]
+        == '{"$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#", "contentVersion": "1.0.0.0", "parameters": {"workspace": {"type": "String"}}, "resources": [{"id": "[concat(resourceId(\'Microsoft.OperationalInsights/workspaces/providers\', parameters(\'workspace\'), \'Microsoft.SecurityInsights\'),\'/alertRules/None\')]", "name": "[concat(parameters(\'workspace\'),\'/Microsoft.SecurityInsights/None\')]", "type": "Microsoft.OperationalInsights/workspaces/providers/alertRules", "kind": "Scheduled", "apiVersion": "2022-09-01-preview", "properties": {"displayName": "Test", "description": "None Technique: .", "severity": "Medium", "enabled": true, "query": "Test_Product\\n| where parse_json(Field1)[\\"value1\\"]==\\"value fajweio\\"", "queryFrequency": "PT30M", "queryPeriod": "PT30M", "triggerOperator": "GreaterThan", "triggerThreshold": 0, "suppressionDuration": "PT2H30M", "suppressionEnabled": true, "tactics": [], "techniques": [], "incidentConfiguration": {"createIncident": true, "groupingConfiguration": {"enabled": false, "reopenClosedIncident": false, "lookbackDuration": "PT2H30M", "matchingMethod": "AllEntities", "groupByEntities": [], "groupByAlertDetails": [], "groupByCustomDetails": []}}, "eventGroupingSettings": {"aggregationKind": "SingleAlert"}, "alertDetailsOverride": null, "customDetails": null, "sentinelEntitiesMappings": null}}]}'
     )
-
-
-# TODO: implement tests for all backend features that don't belong to the base class defaults, e.g. features that were
-# implemented with custom code, deferred expressions etc.
-
-
-def test_alarule_format1_output(alarule_backend: alaruleBackend):
-    """Test for output format format1."""
-    # TODO: implement a test for the output format
-    pass
-
-
-def test_alarule_format2_output(alarule_backend: alaruleBackend):
-    """Test for output format format2."""
-    # TODO: implement a test for the output format
-    pass
-
-
-ala_rule_backend = alaruleBackend()
-test_alarule_field_name_dynamic(ala_rule_backend)
-test_alarule_or_expression(ala_rule_backend)
